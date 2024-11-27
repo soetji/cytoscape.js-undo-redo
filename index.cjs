526239f74728f3e583f7bf1d2f062e16f232e70b
Soetji Anto <soetji@gmail.com>
2024-11-27 14:11:41 -0500

    Add esm export

---
+++ index.cjs
@@ -0,0 +1,20 @@
+import register from './cytoscape-undo-redo';
+
+(function () {
+  'use strict';
+
+  if (typeof module !== 'undefined' && module.exports) { // expose as a commonjs module
+    module.exports = register;
+  }
+
+  if (typeof define !== 'undefined' && define.amd) { // expose as an amd/requirejs module
+    define('cytoscape.js-undo-redo', function () {
+      return register;
+    });
+  }
+
+  if (typeof cytoscape !== 'undefined') { // expose to global cytoscape (i.e. window.cytoscape)
+    register(cytoscape);
+  }
+
+})();
